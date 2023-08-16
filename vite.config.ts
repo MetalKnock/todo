04b667ce9050daf2fcc8a2de991b9e46e05aefab
Dmitry Ivanov <dimannisik1@gmail.com>
2023-08-16 22:47:47 +0700

    build: add browserslist

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src/'),
     },
+  },
+  build: {
+    target: browserslistToEsbuild(),
   },
 });
