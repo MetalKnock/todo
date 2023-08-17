1e53ee808344d9bf3a848302794eb89d9b11f391
Dmitry Ivanov <dimannisik1@gmail.com>
2023-08-17 11:13:07 +0700

    feat: add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
