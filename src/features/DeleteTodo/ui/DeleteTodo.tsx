c82f29429733a9c3bf1bacfc6a8570dbf6816885
Dmitry Ivanov <dimannisik1@gmail.com>
2023-08-31 02:49:24 +0700

    feat: add delete button

---
+++ src/features/DeleteTodo/ui/DeleteTodo.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as DeleteIcon } from '@/shared/assets/icons/delete.svg';
+import { useAppDispatch } from '@/shared/hooks/useAppDispatch';
+import { deleteTodoById } from '@/entities/todo/model/todoSlice';
+import styles from './DeleteTodo.module.scss';
+
+interface DeleteTodoProps {
+  className?: string;
+  id: number;
+}
+
+const DeleteTodo = ({ className, id }: DeleteTodoProps) => {
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(deleteTodoById(id));
+  };
+
+  return <DeleteIcon className={`${styles.DeleteTodo} ${className}`} onClick={handleClick} />;
+};
+
+export default DeleteTodo;
--- src/widgets/TodoList/ui/TodoList.tsx
+++ src/widgets/TodoList/ui/TodoList.tsx
@@ -2,6 +2,7 @@ import { Todo, TodoItem } from '@/entities/todo';
 import styles from './TodoList.module.scss';
 import ToggleTodo from '@/features/ToggleTodo/ui/ToggleTodo';
 import { EditTodo } from '@/features/EditTodo';
+import { DeleteTodo } from '@/features/DeleteTodo';
 
 interface TodoListProps {
   className?: string;
@@ -18,7 +19,12 @@ const TodoList = ({ className, todos }: TodoListProps) => {
             key={id}
             todo={todo}
             leftSlot={<ToggleTodo completed={completed} id={id} />}
-            rightSlot={<EditTodo id={id} title={title} />}
+            rightSlot={
+              <div className={styles.rightSlot}>
+                <EditTodo id={id} title={title} />
+                <DeleteTodo id={id} />
+              </div>
+            }
           />
         );
       })}
